0abd74f1c1c95bc05fa850195ed97b9666c00de0
Livecodebase <livecodebase@gmail.com>
2024-10-23 00:39:42 +0530

    Added compression logic

--- routes/compress/index.js
+++ routes/compress/index.js
@@ -5,13 +5,53 @@ const multerTempUploads = require("../../middleware/multer");
 const fs = require("fs");
 
 router.post("/", multerTempUploads, async (req, res) => {
-  if (!req?.file?.path) {
-   return res.send("error")
-  }
+
   const filepath = req.file.path
   const host = req.headers.host
  
-  res.send("data");
+    const id = shortid.generate();
+    await new Promise(function (resolve, reject) {
+      return fs.readFile(filepath, (err, data) => {
+        sharp(data)
+          .rotate()
+          .toBuffer()
+          .then((mindata) => {
+            (async () => {
+              try {
+                const files = await imagemin.buffer(mindata, {
+                  destination: "public",
+                  plugins: [
+                    imageminMozjpeg({ quality: 80 }),
+                    imageminPngquant({
+                      quality: [0.7, 0.8],
+                    }),
+                  ],
+                });
+                fs.writeFile(`public/${id}.jpeg`, files, function (err) {
+                  if (err) {
+                    throw err;
+                  }
+                  setTimeout(() => {
+                    fs.unlink(`public/${id}.jpeg`, function (err) {
+                      if (err) throw err;
+                    });
+                  }, 100000);
+                  resolve(`http://${host}/${id}.jpeg`);
+                });
+              } catch (error) {
+                return error;
+              }
+            })();
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    });
+  fs.unlink(req.file.path, function (err) {
+    if (err) throw err;
+  });
+  res.send(data);
 });
 
 module.exports = router;
